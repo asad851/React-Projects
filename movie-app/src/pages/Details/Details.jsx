d1ed551a38d96233a67a69bdad62e83e02048163
asad851 <118800128+asad851@users.noreply.github.com>
2023-05-08 08:09:32 +0000

    Details

--- movie-app/src/pages/Details/Details.jsx
+++ movie-app/src/pages/Details/Details.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import DetailsBanner from './DetailsBanner'
+import { useParams } from 'react-router-dom';
+import useFetch from '../../Hooks/useFetch';
+import Cast from './Cast';
+
 
 
 export default function Details() {
+  const { mediaType, id } = useParams();
+    const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+    const { data: credits, loading: creditsLoading } = useFetch(
+        `/${mediaType}/${id}/credits`
+    );
+    // console.log(credits?.data.cast)
+   
+       
   
   return (
     <>
-    <DetailsBanner/>
-    <div className=''>this is details section</div>
+    <DetailsBanner crew={credits?.data?.crew} />
+    <Cast  data={credits?.data?.cast} loading={creditsLoading}/>
+    
     </>
     
   )
--- movie-app/src/pages/Details/DetailsBanner.jsx
+++ movie-app/src/pages/Details/DetailsBanner.jsx
@@ -10,7 +10,7 @@ import Rating from "../../Components/Rating";
 import "./style.css";
 import Playbtn from "./Playbtn";
 
-export default function DetailsBanner() {
+export default function DetailsBanner({ crew }) {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}`);
   const { url } = useSelector((state) => state.home);
@@ -18,9 +18,15 @@ export default function DetailsBanner() {
 
   const moviedata = data?.data;
   const genreName = moviedata?.genres.map((items) => items.id);
+  const watchTime =
+    moviedata?.runtime % 60 === 0
+      ? `${moviedata?.runtime / 60} h `
+      : `${Math.floor(moviedata?.runtime / 60)}h ${moviedata?.runtime % 60}m`;
+  const producers = crew?.find((crew) => crew.job === "Producer");
+  const directors = crew?.find((crew) => crew.job === "Director");
 
-  // console.log(genreName)
   // console.log(moviedata)
+  // console.log(directors);
   const background = url.backdrop + moviedata?.poster_path;
   return (
     <div className="w-full pt-[100px] mb-[50px] min-[768px]:pt-[120px] min-[768px]:mb-0  min-[768px]:h-[700px]">
@@ -71,13 +77,72 @@ export default function DetailsBanner() {
                         className="max-w-[70px] max-h-[70px] bg-[#041226] min-[768px]:max-w-[90px] min-[768px]:max-h-[90px] flex justify-center items-center fill-white   "
                         rating={moviedata?.vote_average.toFixed(1)}
                       />
-                      <div id="play" className="flex items-center gap-[10px] cursor-pointer">
+                      <div
+                        id="play"
+                        className="flex items-center gap-[10px] cursor-pointer"
+                      >
                         <Playbtn />
-                        <span className="text text-[20px] transition-all ease-in-out duration-700">Watch Trailer</span>
+                        <span className="text text-[20px] transition-all ease-in-out duration-700">
+                          Watch Trailer
+                        </span>
                       </div>
                     </div>
                     <div>
-                      
+                      <div className="">
+                        <div className="text-[22px] font-bold">Overview</div>
+                        <div className="leading-[20px] mt-[18px] text-[16px] opacity-[0.5] min-[768px]:pr-[100px] ">
+                          {moviedata?.overview && moviedata?.overview}
+                        </div>
+
+                        <div className="mt-[20px] flex gap-[10px] min-[768px]:gap-[25px] ">
+                          {moviedata?.status && (
+                            <div className="text-[16px] font-semibold ">
+                              Status :{" "}
+                              <span className="text-[16px] min-[768px]:ml-[10px] opacity-[0.5] font-medium ">
+                                {moviedata?.status}{" "}
+                              </span>{" "}
+                            </div>
+                          )}
+                          {moviedata?.release_date && (
+                            <div className="text-[16px] font-semibold">
+                              Relase Date :{" "}
+                              <span className="text-[16px] min-[768px]:ml-[10px] opacity-[0.5] font-medium ">
+                                {dayjs(moviedata?.release_date).format(
+                                  "D MMM YYYY"
+                                )}
+                              </span>
+                            </div>
+                          )}
+                          {moviedata?.runtime && (
+                            <div className="text-[16px] font-semibold">
+                              Watch Time :{" "}
+                              <span className="text-[16px] min-[768px]:ml-[10px] opacity-[0.5] font-medium ">
+                                {watchTime}
+                              </span>{" "}
+                            </div>
+                          )}
+                        </div>
+                        <span className="inline-block w-full h-[0.2px] mt-[20px] opacity-[0.3] bg-white"></span>
+                        {producers&&    <>
+
+                        <div className="text-[17px] font-semibold mt-[10px]">
+                          Producer :{" "}
+                          <span className=" min-[768px]:ml-[10px] font-medium text-[16px] opacity-[0.5]  ">
+                            {producers?.name}
+                          </span>
+                        </div>
+                        <span className="inline-block w-full h-[0.2px]  opacity-[0.3] bg-white"></span>
+                         </>}
+
+                        
+                        {directors&&<><div className="text-[17px] font-semibold mt-[10px]">
+                          Director :{" "}
+                          <span className=" min-[768px]:ml-[10px] font-medium text-[16px] opacity-[0.5]  ">
+                            {directors?.name}
+                          </span>
+                        </div>
+                        <span className="inline-block w-full h-[0.2px]  opacity-[0.3] bg-white"></span></>}
+                      </div>
                     </div>
                   </div>
                 </div>
