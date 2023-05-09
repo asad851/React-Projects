0858df57ea0a8bc35b58424f678a98e942fa5e42
asad851 <118800128+asad851@users.noreply.github.com>
2023-05-09 11:39:22 +0000

    new

--- movie-app/src/pages/SearchResult.jsx
+++ movie-app/src/pages/SearchResult.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useFetch from "../Hooks/useFetch";
 import { useNavigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -7,16 +7,23 @@ import Rating from "../Components/Rating";
 import Genre from "../Components/Genre";
 import dayjs from "dayjs";  
 import noposter from "../assets/no-poster.png"
+import InfiniteScroll from "react-infinite-scroll-component";
+import Spinner from "../Components/Spinner";
+import { fetchData } from "../Api";
+
 
 
 
 
 export default function SearchResult() {
+  const [loading , setLoading] = useState(false)
+  const [data, setData] = useState([])
+  const [pageNum, setPageNum] = useState(1)
   const { query } = useParams();
   const navigate = useNavigate()
-  const{data,loading} = useFetch(`/search/multi?query=${query}&page=1`)
+  // 
   const {url} = useSelector((state)=>state.home)
-  console.log(data?.data?.results)
+  console.log(data?.data)
   const Search =data?.data?.results
   const skeleton = () => {
     return (
@@ -29,23 +36,61 @@ export default function SearchResult() {
       </div>
     );
   };
+  const fetchInitialPage=()=>{
+    setLoading(true)
+    fetchData(`/search/multi?query=${query}&page=${pageNum}`).then(
+      (res)=>{
+        setData(res?.data)
+        setLoading(false)
+        setPageNum((prev)=>prev+1)
+      })
+  }
+
+  const fetchNextPage=()=>{
+    setLoading(true)
+    fetchData(`/search/multi?query=${query}&page=${pageNum}`).then(
+      (res)=>{
+        if(data?.results){
+          setData([...data,...res?.data]);
+        }else{
+          setData(res?.data)
+        }
+        setPageNum((prev) => prev + 1);
+      })
+  }
+  
+    useEffect(() => {
+      setPageNum(1);
+      fetchInitialPage();
+  }, [query]);
+
+  
   return (
     <div className="max-w-[1200px] w-full px-[20px] mx-auto min-[768px]:pt-[120px]  pt-[60px] ">
-      {!loading ? (
+      {loading&&<Spinner/>}
+      {!loading ? (<>
+           <div className="text-white text-[18px] mb-[15px]">{`Showing ${Search?.length<=1?"result":"results"} for "${query}" `}</div>
           <div
             // ref={CarouselContainer}
-            className="flex gap-[20px] justify-center  flex-wrap mr-[-20px] ml-[-20px] px-[20px] min-[768px]:gap-[20px] min-[768px]:overflow-hidden min-[768px]:m-0 min-[768px]:p-0 items-center h-full"
+            className="w-full h-full"
           >
-            {Search?.map((item) => {
-              const postUrl = item.poster_path
-                ? url.backdrop + item.poster_path
+            <InfiniteScroll
+            className="flex gap-[20px] justify-center  flex-wrap mr-[-20px] ml-[-20px] px-[20px] min-[768px]:gap-[20px] min-[768px]:overflow-hidden min-[768px]:m-0 min-[768px]:p-0 items-center h-full"
+            dataLength={data?.results?.length || []}
+            next={fetchNextPage}
+            hasMore={pageNum <= data?.total_pages}
+            loader={<Spinner />}
+            >
+            {data?.results?.map((item) => {
+              const postUrl = item?.poster_path
+                ? url.backdrop + item?.poster_path
                 : noposter;
               return (
                 <div
                   key={item.id}
                   className="w-[calc(48%-10px)]   cursor-pointer shrink-0 min-[768px]:w-[calc(25%-15px)] lg:w-[calc(20%-16px)] h-full py-[10px] hover:scale-[1.02] duration-200 ease-linear "
                   onClick={() =>
-                    navigate(`/${item.media_type || endpoint}/${item.id}`)
+                    navigate(`/${item?.media_type || endpoint}/${item?.id}`)
                   }
                 >
                   <div className="w-full h-full aspect-[1/1.5] mb-[20px] relative  flex items-end justify-between ">
@@ -54,7 +99,7 @@ export default function SearchResult() {
                       className="rounded-[12px] h-full  w-full top-0 left-0 overflow-hidden bg-center object-center object-cover "
                     />
                     <div className="flex justify-between">
-                    <Rating classCarousel=" bg-white absolute bottom-[-15px]  left-[20px]  h-[43px] w-[43px] min-[768px]:w-[53px] min-[768px]:h-[53px] flex justify-center items-center max-[768px]:hidden" rating={item.vote_average.toFixed(1)} />
+                    <Rating classCarousel=" bg-white absolute bottom-[-15px]  left-[20px]  h-[43px] w-[43px] min-[768px]:w-[53px] min-[768px]:h-[53px] flex justify-center items-center max-[768px]:hidden" rating={item?.vote_average?.toFixed(1)} />
                     <Genre 
                       classNamecarousel="flex-col absolute bottom-[20px] right-[20px] hidden justify-end min-[912px]:flex  flex-wrap  "
                       data={item?.genre_ids?.slice(0, 2)}
@@ -63,22 +108,25 @@ export default function SearchResult() {
                   </div>
                   <div className="flex text-white flex-col ">
                     <span className="text-[16px] text-white  mb-[10] leading-[24px] min-[768px]:text-[20px] truncate">
-                      {item.title || item.name}
+                      {item?.title || item?.name}
                     </span>
                     <span className="text-[13px] opacity-[0.5]">
-                      {dayjs(item.release_Date).format("D MMM, YYYY")}
+                      {dayjs(item?.release_Date).format("D MMM, YYYY")}
                     </span>
                   </div>
                 </div>
-              );
-            })}
-          </div>):(<div className="flex gap-[10px] overflow-y-hidden  mx-[-20px] px-[20px] min-[768px]:gap-[20px] min-[768px]:overflow-hidden min-[768px]:m-0 min-[768px]:p-0">
+                
+                );
+              })}
+              </InfiniteScroll>
+          </div></>):(<div className="flex gap-[10px] overflow-y-hidden  mx-[-20px] px-[20px] min-[768px]:gap-[20px] min-[768px]:overflow-hidden min-[768px]:m-0 min-[768px]:p-0">
             {skeleton()}
             {skeleton()}
             {skeleton()}
             {skeleton()}
             {skeleton()}
           </div>)}
+          
     </div>
      ); 
 }
