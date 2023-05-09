4fb370e42999b0d2e6fbbb28b616b89253750ed4
asad851 <118800128+asad851@users.noreply.github.com>
2023-05-09 12:10:49 +0000

    new

--- movie-app/src/pages/SearchResult.jsx
+++ movie-app/src/pages/SearchResult.jsx
@@ -68,57 +68,57 @@ export default function SearchResult() {
   return (
     <div className="max-w-[1200px] w-full px-[20px] mx-auto min-[768px]:pt-[120px]  pt-[60px] ">
       {loading&&<Spinner/>}
-      {!loading ? (<>
+      {!loading ? (<> 
            <div className="text-white text-[18px] mb-[15px]">{`Showing ${Search?.length<=1?"result":"results"} for "${query}" `}</div>
           <div
             // ref={CarouselContainer}
             className="w-full h-full"
           >
-            <InfiniteScroll
-            className="flex gap-[20px] justify-center  flex-wrap mr-[-20px] ml-[-20px] px-[20px] min-[768px]:gap-[20px] min-[768px]:overflow-hidden min-[768px]:m-0 min-[768px]:p-0 items-center h-full"
-            dataLength={data?.results?.length || []}
-            next={fetchNextPage}
-            hasMore={pageNum <= data?.total_pages}
-            loader={<Spinner />}
-            >
-            {data?.results?.map((item) => {
-              const postUrl = item?.poster_path
-                ? url.backdrop + item?.poster_path
-                : noposter;
-              return (
-                <div
-                  key={item.id}
-                  className="w-[calc(48%-10px)]   cursor-pointer shrink-0 min-[768px]:w-[calc(25%-15px)] lg:w-[calc(20%-16px)] h-full py-[10px] hover:scale-[1.02] duration-200 ease-linear "
-                  onClick={() =>
-                    navigate(`/${item?.media_type || endpoint}/${item?.id}`)
-                  }
-                >
-                  <div className="w-full h-full aspect-[1/1.5] mb-[20px] relative  flex items-end justify-between ">
-                    <Img
-                      src={postUrl}
-                      className="rounded-[12px] h-full  w-full top-0 left-0 overflow-hidden bg-center object-center object-cover "
-                    />
-                    <div className="flex justify-between">
-                    <Rating classCarousel=" bg-white absolute bottom-[-15px]  left-[20px]  h-[43px] w-[43px] min-[768px]:w-[53px] min-[768px]:h-[53px] flex justify-center items-center max-[768px]:hidden" rating={item?.vote_average?.toFixed(1)} />
-                    <Genre 
-                      classNamecarousel="flex-col absolute bottom-[20px] right-[20px] hidden justify-end min-[912px]:flex  flex-wrap  "
-                      data={item?.genre_ids?.slice(0, 2)}
+              {data?.results?.length<0?(<InfiniteScroll
+              className="flex gap-[20px] justify-center  flex-wrap mr-[-20px] ml-[-20px] px-[20px] min-[768px]:gap-[20px] min-[768px]:overflow-hidden min-[768px]:m-0 min-[768px]:p-0 items-center h-full"
+              dataLength={data?.results?.length || []}
+              next={fetchNextPage}
+              hasMore={pageNum <= data?.total_pages}
+              loader={<Spinner />}
+              >
+              {data?.results?.map((item) => {
+                const postUrl = item?.poster_path
+                  ? url.backdrop + item?.poster_path
+                  : noposter;
+                return (
+                  <div
+                    key={item.id}
+                    className="w-[calc(48%-10px)]   cursor-pointer shrink-0 min-[768px]:w-[calc(25%-15px)] lg:w-[calc(20%-16px)] h-full py-[10px] hover:scale-[1.02] duration-200 ease-linear "
+                    onClick={() =>
+                      navigate(`/${item?.media_type || endpoint}/${item?.id}`)
+                    }
+                  >
+                    <div className="w-full h-full aspect-[1/1.5] mb-[20px] relative  flex items-end justify-between ">
+                      <Img
+                        src={postUrl}
+                        className="rounded-[12px] h-full  w-full top-0 left-0 overflow-hidden bg-center object-center object-cover "
                       />
-                      </div>
+                      <div className="flex justify-between">
+                      <Rating classCarousel=" bg-white absolute bottom-[-15px]  left-[20px]  h-[43px] w-[43px] min-[768px]:w-[53px] min-[768px]:h-[53px] flex justify-center items-center max-[768px]:hidden" rating={item?.vote_average?.toFixed(1)} />
+                      <Genre 
+                        classNamecarousel="flex-col absolute bottom-[20px] right-[20px] hidden justify-end min-[912px]:flex  flex-wrap  "
+                        data={item?.genre_ids?.slice(0, 2)}
+                        />
+                        </div>
+                    </div>
+                    <div className="flex text-white flex-col ">
+                      <span className="text-[16px] text-white  mb-[10] leading-[24px] min-[768px]:text-[20px] truncate">
+                        {item?.title || item?.name}
+                      </span>
+                      <span className="text-[13px] opacity-[0.5]">
+                        {dayjs(item?.release_Date).format("D MMM, YYYY")}
+                      </span>
+                    </div>
                   </div>
-                  <div className="flex text-white flex-col ">
-                    <span className="text-[16px] text-white  mb-[10] leading-[24px] min-[768px]:text-[20px] truncate">
-                      {item?.title || item?.name}
-                    </span>
-                    <span className="text-[13px] opacity-[0.5]">
-                      {dayjs(item?.release_Date).format("D MMM, YYYY")}
-                    </span>
-                  </div>
-                </div>
-                
-                );
-              })}
-              </InfiniteScroll>
+                  
+                  );
+                })}
+                </InfiniteScroll>):(<div className=" w-full text-[25px] text-white font-medium text-center"> Sorry the searched movie/Tvshow couldn't be found</div>)}
           </div></>):(<div className="flex gap-[10px] overflow-y-hidden  mx-[-20px] px-[20px] min-[768px]:gap-[20px] min-[768px]:overflow-hidden min-[768px]:m-0 min-[768px]:p-0">
             {skeleton()}
             {skeleton()}
