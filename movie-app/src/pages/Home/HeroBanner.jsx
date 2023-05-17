640dd18c8d358d0523e3de3515607151c0e47571
asad851 <siddiquiasad851@gmail.com>
2023-05-17 23:47:08 +0530

    commit

--- movie-app/src/pages/Home/HeroBanner.jsx
+++ movie-app/src/pages/Home/HeroBanner.jsx
@@ -1,39 +1,45 @@
 import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import React from "react";
 import Img from "../../Components/Img";
 import useFetch from "../../Hooks/useFetch";
 import { useNavigate } from "react-router-dom";
+import Playbtn from "../Details/Playbtn";
+import { VscAdd } from "react-icons/vsc";
+import {VscInfo} from "react-icons/vsc"
+
+
 
 export default function HeroBanner() {
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
   const { data, loading } = useFetch("/movie/upcoming");
+  const [randomData, setRandomData] = useState(null);
   const { url } = useSelector((state) => state.home);
- 
+  const {myListArr} =useSelector(state=>state.mylist)
   
-  useEffect(() => {
-    
-      let bg =
-      url.backdrop +
-      data?.data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
-    setBackground(bg);
-    
-  }, [data]);
 
+  useEffect(() => {
+    let a= data?.data?.results?.[Math.floor(Math.random() * 20)];
+    setRandomData(a)
+    let bg = url.backdrop + a?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
+  // console.log(randomData)
+  const { data: moviedata } = useFetch(`/movie/${randomData?.id}`);
+  console.log(moviedata)
   const searchQueryHandler = (e) => {
     e.preventDefault();
     if (e.key === "Enter" && query.length > 0) {
       navigate(`/search/${query}`);
     }
-  
-};
-const handleSubmit=()=>{
+  };
+  const handleSubmit = () => {
     if (query.length > 0) {
-      navigate(`/search/${query}`)};
-
-  }
+      navigate(`/search/${query}`);
+    }
+  };
 
   return (
     <div className="w-full ">
@@ -47,7 +53,37 @@ const handleSubmit=()=>{
           </div>
         )}
         <div className=" opacitylayer w-full h-[250px] absolute bottom-0 left-0"></div>
-        <div className="w-full my-0 py-0 mx-auto px-[20px] ">
+        {/* <div
+          id="Circlerating"
+          className="flex items-center justify-start gap-[15px] mt-[20px] mb-[20px]"
+        >
+          <Rating
+            className="max-w-[70px] max-h-[70px] bg-[rgba(10,10,14)] min-[768px]:max-w-[90px] min-[768px]:max-h-[90px] flex justify-center items-center fill-white   "
+            rating={moviedata?.data?.vote_average.toFixed(1)}
+          />
+          <div
+            id="play"
+            className="flex items-center gap-[10px] min-[768px]:gap-[20px] cursor-pointer"
+          >
+            <Playbtn height={height} width={width} />
+            <span className="text-[16px] min-[768px]:text-[20px] transition-all ease-in-out duration-700 hover:text-[rgba(67,137,216)]">
+              Watch Trailer
+            </span>
+          </div>
+          
+          <div className="flex overflow-hidden flex-wrap  justify-center items-center gap-[10px] cursor-pointer  ">
+            <VscAdd
+              className={`font-extrabold text-[30px] min-[768px]:text-[40px] transition-[transform] duration-500 ease-in-out ${
+                clicked ? "rotate-[135deg] hover:text-red-600" : ""
+              } hover:text-[rgba(67,137,216)]`}
+              onClick={handleAddToList}
+            />
+            <span className="text-[16px]  min-[768px]:text-[20px] transition-colors ease-in duration-500 ">
+              {clicked ? "Remove" : "Add"}
+            </span>
+          </div>
+        </div> */}
+        {/* <div className="w-full my-0 py-0 mx-auto px-[20px] ">
           <div className="flex justify-center items-center flex-col text-white mx-auto max-w-[800px]  relative">
             <span className="text-[50px] min-[768px]:text-[90px]   font-bold mb-[10px]">
               Welcome
@@ -74,7 +110,7 @@ const handleSubmit=()=>{
               </button>
             </form>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
